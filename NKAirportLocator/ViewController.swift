107697102e76ccd1fff18ba40b74499155265629
Nikhil Sharma <nisharma@dminc.com>
2020-02-21 20:15:44 +0530

    UNTRACKED: Project Initiated

---
+++ NKAirportLocator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NKAirportLocator
+//
+//  Created by Nikhil Sharma on 21/02/20.
+//  Copyright © 2020 AirAsiaSubmission. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
